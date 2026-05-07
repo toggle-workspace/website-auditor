876d19e4035f9d946cd5ba12b499a1a8b41d517d
aizadrzo <aizadridzoo@gmail.com>
2026-05-07 09:55:25 +0800

    fix-cloudflare-issues

---
+++ .open-next/cloudflare/next-env.mjs
@@ -0,0 +1,3 @@
+export const production = {"PAGESPEED_API_KEY":"AIzaSyCs4W0Zvo7dNz6lfgwNK1SodlbEjktR8vA","OPEN_PAGERANK_API_KEY":"oco08gsow400w0ksk0ss0ock8k4o8kkww8owg4s4"};
+export const development = {"PAGESPEED_API_KEY":"AIzaSyCs4W0Zvo7dNz6lfgwNK1SodlbEjktR8vA","OPEN_PAGERANK_API_KEY":"oco08gsow400w0ksk0ss0ock8k4o8kkww8owg4s4"};
+export const test = {};
